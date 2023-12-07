e2d12634e76214c67d3874cdb7c39adce08e0f78
kotathefriends <5070kouta@gmail.com>
2023-12-07 14:28:52 +0900

    ビューの作成と結合まで

---
+++ Landmarks/Views/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by KOTA TAKAHASHI on 2023/12/07.
+//
+
+import SwiftUI
+
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+
+#Preview {
+    CircleImage()
+}
